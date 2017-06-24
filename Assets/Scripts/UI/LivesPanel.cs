85104cf6cb16e1a118bb5c25d4fed7be74d84feb
zmtrvch <miumiu1078@gmail.com>
2017-06-24 14:46:32 +0300

    LivesPanel added

---
+++ Assets/Scripts/UI/LivesPanel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LivesPanel : MonoBehaviour {
+	public List<UI2DSprite> lifes;
+	public Sprite empty_life;
+	public Sprite full_life;
+	int maxLifesNumber = 3;
+
+	void FixedUpdate () {
+		setLives (); 
+		controlLives (); 
+	}
+
+	void controlLives() {
+		for (int i =0; i < maxLifesNumber; ++i)
+			if (i == LevelController.current.getLifes ())
+				lifes [i].sprite2D = empty_life;
+	} 
+	public void setLives(){
+		if (LevelController.current.getLifes () == maxLifesNumber) {
+			for (int i = 0; i < maxLifesNumber; ++i) {
+				lifes [i].sprite2D = full_life;
+
+			}
+		}
+	}
+
+}
