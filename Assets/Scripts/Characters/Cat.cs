b43c85e95b01ef2240214bfa0873fdb26c0c0a96
zmtrvch <miumiu1078@gmail.com>
2017-06-25 13:41:37 +0300

    Cat jump fixed

--- Assets/Scripts/Characters/Cat.cs
+++ Assets/Scripts/Characters/Cat.cs
@@ -42,59 +42,80 @@ public class Cat : MonoBehaviour {
 	}
 	void FixedUpdate () {
 		float value = Input.GetAxis ("Horizontal");
-		if (SceneManager.GetActiveScene ().name == "MainMenu") {
-			Debug.Log ("MainMenu");
+
+		Animator animator = GetComponent<Animator> ();
+
+		if(Mathf.Abs(value) > 0) {
+			animator.SetBool ("run", true);
 		} else {
-			Animator animator = GetComponent<Animator> ();
-			if (Mathf.Abs (value) > 0) {
-				animator.SetBool ("run", true);
-			} else {
-				animator.SetBool ("run", false);
-			}
-
-			if (Mathf.Abs (value) > 0) {
-				Vector2 vel = myBody.velocity;
-				vel.x = value * speed;
-				myBody.velocity = vel;
-			}
-
-			SpriteRenderer sr = GetComponent<SpriteRenderer> ();
-			if (value < 0) {
-				sr.flipX = true;
-			} else if (value > 0) {
-				sr.flipX = false;
-			
-				Vector3 from = transform.position + Vector3.up * 0.3f;
-				Vector3 to = transform.position + Vector3.down * 0.1f;
-		
-				if (Input.GetButtonDown ("Jump") && isGrounded) {
-					this.JumpActive = true;
-				}
-				if (this.JumpActive) {
-					if (Input.GetButton ("Jump")) {
-						this.JumpTime += Time.deltaTime;
-						if (this.JumpTime < this.MaxJumpTime) {
-							Vector2 vel = myBody.velocity;
-							vel.y = JumpSpeed * (1.0f - JumpTime / MaxJumpTime);
-							myBody.velocity = vel;
-
-						} else {
-							this.JumpActive = false;
-							this.JumpTime = 0;
-						}
-					}
-
-					if (this.isGrounded) {
-						animator.SetBool ("jump", false);
-					} else {
-						animator.SetBool ("jump", true);
-					}
-				}
-			}
-
-
-
+			animator.SetBool ("run", false);
 		}
+
+		if (Mathf.Abs (value) > 0) {
+			Vector2 vel = myBody.velocity;
+			vel.x = value * speed;
+			myBody.velocity = vel;
+		}
+
+		SpriteRenderer sr = GetComponent<SpriteRenderer>();
+		if (value < 0)
+		{
+			sr.flipX = true;
+		}
+		else if (value > 0)
+		{
+			sr.flipX = false;
+		}
+		Vector3 from = transform.position + Vector3.up * 0.3f;
+		Vector3 to = transform.position + Vector3.down * 1.6f;
+		int layer_id = 1 << LayerMask.NameToLayer("Ground");
+		//Перевіряємо чи проходить лінія через Collider з шаром Ground
+		RaycastHit2D hit = Physics2D.Linecast(from, to, layer_id);
+		if (hit)
+		{
+			isGrounded = true;
+		}
+		else
+		{
+			isGrounded = false;
+		}
+
+		//Намалювати лінію (для розробника)
+		Debug.DrawLine(from, to, Color.red);
+
+		if (Input.GetButtonDown("Jump") && isGrounded)
+		{
+			this.JumpActive = true;
+		}
+		if (this.JumpActive)
+		{
+			//Якщо кнопку ще тримають
+			if (Input.GetButton("Jump"))
+			{
+				this.JumpTime += Time.deltaTime;
+				if (this.JumpTime < this.MaxJumpTime)
+				{
+					Vector2 vel = myBody.velocity;
+					vel.y = JumpSpeed * (1.0f - JumpTime / MaxJumpTime);
+					myBody.velocity = vel;
+				}
+			}
+			else
+			{
+				this.JumpActive = false;
+				this.JumpTime = 0;
+			}
+		}
+
+		if (this.isGrounded)
+		{
+			animator.SetBool("jump", false);
+		}
+		else
+		{
+			animator.SetBool("jump", true);
+		}
+
 	}
 		public void addHealth(int number)
 		{
