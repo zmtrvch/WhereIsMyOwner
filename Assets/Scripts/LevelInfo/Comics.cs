401f429255ac542f449aadf2cade91db55eca216
zmtrvch <miumiu1078@gmail.com>
2017-06-26 21:03:55 +0300

    Comics scene finished, pizza

---
+++ Assets/Scripts/LevelInfo/Comics.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Comics : MonoBehaviour {
+
+
+	void Start () {
+		StartCoroutine (comics(2.0f));
+		
+	}
+	
+	public IEnumerator comics(float time){
+		yield return new WaitForSeconds (time);
+		SceneManager.LoadScene ("Levels");
+	}
+}
--- Assets/Scripts/UI/PlayButton.cs
+++ Assets/Scripts/UI/PlayButton.cs
@@ -1,12 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine;
 using UnityEngine.SceneManagement;
 public class PlayButton : MonoBehaviour {
 
 	public void _onClick()
 	{
-		SceneManager.LoadScene ("Levels");
+		SceneManager.LoadScene ("Slideshow");
 	}
 }
